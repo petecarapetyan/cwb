760d62b69b56964b37809ff3d0421cde66ca7f16
petecarapetyan <pete@datafundamentals.com>
2021-08-12 10:33:22 -0500

    WIP

--- packages/cart/src/ui/app-shell.ts
+++ packages/cart/src/ui/app-shell.ts
@@ -2,6 +2,7 @@ import { html, css} from 'lit';
 import { Connected, State, AuthSelectors } from "./connected";
 import {customElement, property} from 'lit/decorators.js';
 import { sharedStyles } from "./shared-styles";
+import { cssVars } from './css-vars';
 
 import './auth-status'
 import "./view-interest-C";
@@ -34,12 +35,40 @@ export class AppShellElement extends Connected {
     <hr/>
     <p>Click the "Update Cart" button to bring any products from localstorage into the cart</p>
     
-    <view-interest-rd></view-interest-rd>` :
+    <view-interest-rd></view-interest-rd>
+    
+    <hr />
+          <div if-user>
+            <input type="checkbox" id="user" name="user" checked disabled/>
+            <label for="user">User</label>
+          </div>
+          <div if-not-user>
+            <input type="checkbox" id="notuser" name="notuser" disabled/>
+            <label for="notuser">User</label></div>
+          <div if-moderator>
+            <input type="checkbox" id="moderator" name="moderator" checked disabled/>
+            <label for="moderator">Moderator</label></div>
+          <div if-not-moderator>
+            <input type="checkbox" id="notmoderator" name="notmoderator" disabled/>
+            <label for="notmoderator">Moderator</label></div></div>
+          <div if-admin>
+            <input type="checkbox" id="admin" name="admin" checked disabled/>
+            <label for="admin">Admin</label></div>
+          <div if-not-admin>
+            <input type="checkbox" id="notadmin" name="notadmin" disabled/>
+            <label for="notadmin">Admin</label></div></div>
+          <div if-superadmin>
+            <input type="checkbox" id="superadmin" name="superadmin" checked disabled/>
+            <label for="superadmin">Super Admin</label></div>
+          <div if-not-superadmin>
+            <input type="checkbox" id="notsuperadmin" name="notsuperadmin" disabled/>
+            <label for="notsuperadmin">Super Admin</label></div></div>
+          <hr />` :
       html`<a href="/signin">Sign In, first</a>`
   }
 
   static get styles() {
-    return [sharedStyles, 
+    return [sharedStyles, cssVars,
     css`
       :host {
         padding: 2em;
